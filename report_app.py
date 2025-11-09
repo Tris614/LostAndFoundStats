571ccb9a45fec98588e47b2bba9ed39bd3e020ff
Tris614 <tristanc6144@gmail.com>
2025-11-09 16:26:32 +0200

    changed to use CreatedDate not datelost

--- report_app.py
+++ report_app.py
@@ -152,8 +152,8 @@ def load_items(start_dt, end_dt):
     sql = """
     SELECT ItemId, UserId, Title, LostDescription, Category, Location, DateLost, Status, CreatedBy, CreatedDate
     FROM Items
-    WHERE DateLost BETWEEN ? AND ?
-    ORDER BY DateLost DESC
+    WHERE CreatedDate BETWEEN ? AND ?
+    ORDER BY CreatedDate DESC
     """
 
     return db_helper.query_to_df(sql, params=(start_dt, end_dt))
@@ -197,19 +197,19 @@ def to_excel_bytes(dfs: dict) -> bytes:
 def prep_export(report_type: str, items_filtered):
     if report_type == "All":
         sheets = {
-            "Lost Items": items_filtered[items_filtered["Status"] == 0].sort_values("DateLost"),
-            "Found Items": items_filtered[items_filtered["Status"] == 1].sort_values("DateLost"),
-            "Claims": items_filtered[items_filtered["Status"] == 2].sort_values("DateLost")
+            "Lost Items": items_filtered[items_filtered["Status"] == 0].sort_values("CreatedDate"),
+            "Found Items": items_filtered[items_filtered["Status"] == 1].sort_values("CreatedDate"),
+            "Claims": items_filtered[items_filtered["Status"] == 2].sort_values("CreatedDate")
         }
     elif report_type == "Lost":
         sheets = {
-            "Lost Items": items_filtered[items_filtered["Status"] == 0].sort_values("DateLost")}
+            "Lost Items": items_filtered[items_filtered["Status"] == 0].sort_values("CreatedDate")}
     elif report_type == "Found":
         sheets = {
-            "Found Items": items_filtered[items_filtered["Status"] == 1].sort_values("DateLost")}
+            "Found Items": items_filtered[items_filtered["Status"] == 1].sort_values("CreatedDate")}
     elif report_type == "Claims":
         sheets = {
-            "Claims": items_filtered[items_filtered["Status"] == 2].sort_values("DateLost")}
+            "Claims": items_filtered[items_filtered["Status"] == 2].sort_values("CreatedDate")}
     else:
         sheets = {}
     return sheets
@@ -235,6 +235,7 @@ if st.button("Generate Report"):
             file_name=fin_file,
             mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
         )
+
 
 
 
